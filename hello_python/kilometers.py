0d057352322904e67c99c206273f2720c72439ef
emmiechege <emilywanjachege@gmail.com>
2023-11-30 12:21:45 +0300

    converting miles to kilometers

--- hello_python/kilometers.py
+++ hello_python/kilometers.py
@@ -4,6 +4,9 @@
 # Enter Miles 5( to be inputted)
 # 5 miles equals 8.04 kilometers(output)
 miles = input("Enter miles: ")
+# convert miles to float
 miles = float(miles)
+# convert miles to float
 kilometers = miles * (1.60934)
+# print the formatted output
 print("{} miles equals to {} kilometers".format(miles, kilometers))
