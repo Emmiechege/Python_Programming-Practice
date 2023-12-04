f40ddf520878aaa436b4725f76a77cee3d57e742
emmiechege <emilywanjachege@gmail.com>
2023-12-04 12:04:13 +0300

    more on logical opetators

--- hello_python/logic_ops.py
+++ hello_python/logic_ops.py
@@ -7,7 +7,7 @@
 age = eval(input("Enter the age: "))
 if (age <= 5):
     print("Go to kindergatten")
-elif (age >= 6) and (age <= 17):
+elif (age > 5) and (age <= 17):
     print("Go to elementary")
 elif not (age > 17) or (age <= 25):
     print("Go to college")
