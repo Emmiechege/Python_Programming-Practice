2bd80dcfd29a5eac89e5fc37edfed49cd5aa1acc
Emmiechege <emilywanjachege@gmail.com>
2024-01-24 17:29:36 +0300

    list of transportation modes

---
+++ lists/transport.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+transport = ['bicycle', 'motor', 'truck', 'plane', 'train']
+message_1 = ("I love riding in a " + transport[1].title() + ".")
+message_2 = ("I would love to board a " + transport[3].title() + ".")
+print(message_1 + message_2 + "It would be so fun.")
