6e746ffa18fcf6c8d74dcfb772f668ee8f92ede5
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 10:34:37 +0300

    looping through a list of magicians with for loop

---
+++ for_loops/magician.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+magicians = ['terry', 'ann', 'robert', 'joe']
+for magician in magicians:
+    print(magician.title() + ", I attended your show.")
+    print("See you soon," + magician.title() + ".\n")
+    print("Thank you all for your co-operation.")
