e429244775053a489ad1fb48198166db2df50f83
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 17:24:26 +0300

    copying items from one list to another

--- lists/slicing/foods.py
+++ lists/slicing/foods.py
@@ -3,4 +3,7 @@ my_foods = ['rice', 'ugali', 'chicken', 'pilau']
 friends_food = my_foods[:]
 print("My favorite food is:")
 for food in my_foods:
-    print(food.title())
+    print(food)
+print("\nMy friends favorite foods are :")
+for her_food in friends_food:
+    print(her_food)
