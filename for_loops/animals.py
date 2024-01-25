54efb1cd08a54b85838a4ef522725e9b681b92cd
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 10:57:56 +0300

    Using a for loop to go though a list of animals

--- for_loops/animals.py
+++ for_loops/animals.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 animals = ['cheetah', 'tiger', 'leoplard', 'panther', 'lion', 'jaguar']
 for animal in animals:
-    print("A " + animal.title() + " would make a great pet.\n")
+    print("A " + animal.title() + " would make a great pet.")
+    print(animal.title() + " is a member of the cat family.")
+
