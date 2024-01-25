b4a66672076bc3c7a043aca19e4c55dd053ea8cb
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 10:36:26 +0300

    looping through pizza types using a for loop

---
+++ for_loops/pizza.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+pizzas = ['hawaaian', 'margherita', 'bbq chicken', 'peperoni']
+for pizza in pizzas:
+    print(pizza.title() + " pizza is found in kenyan restaurants.")
+    print("Most preffered pizza is " + pizza.title() + ".")
+    print("We all love pizza.\n")
