94802a38fd1b031ecda8fb8a83dcd97a0ff9948a
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 21:00:13 +0300

    writing over tuples

---
+++ lists/tuples/shoes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+shoes = ('sandals', 'boots', 'heels')
+print("List of old shoes:")
+for shoe in shoes:
+    print(shoe)
+shoes = ('sneakers', 'dollies', 'strappy')
+print("\nList of new shoes:")
+for shoe in shoes:
+    print(shoe)
