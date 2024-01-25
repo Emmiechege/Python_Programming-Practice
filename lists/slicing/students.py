2cfcea33f5c6b5398cde634062de023bdae0f897
Emmiechege <emilywanjachege@gmail.com>
2024-01-25 16:22:08 +0300

    gets the list of first three students using slicing

--- lists/slicing/students.py
+++ lists/slicing/students.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 students = ['eunice', 'rose', 'dennis', 'peter', 'andrew', 'cate']
-print("The fist three items in the list are:")
-print(students[:3])
+print("The fist three students in the list are:")
+for student in students[:3]:
+    print(student.title())
