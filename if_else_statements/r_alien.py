f875a0c6a18d8c448a495199e06fde6924fe35a7
Emmiechege <emilywanjachege@gmail.com>
2024-02-03 09:13:31 +0300

    if-elif aliens

---
+++ if_else_statements/r_alien.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+alien_color = 'blue'
+if alien_color == 'red':
+    print("It is a red alien")
+elif alien_color == 'pink':
+    print("It is a pink alien")
+else:
+    print("THis is the correct alien color")
