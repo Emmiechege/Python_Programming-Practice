f8d8b5137b86b393f0e094461456eedc3eb96ad2
Emmiechege <emilywanjachege@gmail.com>
2024-02-03 06:15:34 +0300

    an if elif, else block

---
+++ if_else_statements/alien.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+alien_color = 'green'
+if alien_color == 'yellow':
+    print("it is yellow")
+elif alien_color == 'red':
+    print("it is red")
+else:
+    print("You earned 5 points")
